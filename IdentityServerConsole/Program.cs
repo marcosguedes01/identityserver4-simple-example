a56621c166f813be4a25a3053bf6067e20545dc9
Marcos Guedes <mguedes@growin.pt>
2019-07-20 21:54:57 +0100

    Arquivos Iniciais

---
+++ IdentityServerConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using IdentityModel;
+using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IdentityServerConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:44303");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+        }
+    }
+}
